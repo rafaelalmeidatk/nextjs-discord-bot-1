17f0ba086b6b17196aefb88cc033ab6b2a2101ba
Vũ Văn Dũng <me@joulev.dev>
2023-10-08 02:30:46 +0700

    Add uid of the reported user to the log message content (#5)

--- src/commands/context/dont-ask-to-ask.ts
+++ src/commands/context/dont-ask-to-ask.ts
@@ -5,6 +5,6 @@ export const command = createReplyableMessageCommand({
   reply: {
     title: "Don't ask to ask, just ask!",
     content:
-      "Please ask your question directly. If someone knows the answer, they will reply. If you don't get an answer, you can try again later.\n\nAlso see: <https://dontasktoask.com/>",
+      'Please just ask your question directly: <https://dontasktoask.com/>',
   },
 });
