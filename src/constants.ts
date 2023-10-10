743f29762acefe9b2bc6b30c1118e65629ed3e5e
Rafael Almeida <rafaelalmeidatk@gmail.com>
2023-10-09 23:28:39 -0300

    Fix showcase channel ID

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 // Channel IDs that don't need to change for each environment
-export const SHOWCASE_CHANNEL_ID = '1159354268353036349';
+export const SHOWCASE_CHANNEL_ID = '1159349244256342027';
 export const CONTENT_SHOWCASE_CHANNEL_ID = '1159354268353036349';
