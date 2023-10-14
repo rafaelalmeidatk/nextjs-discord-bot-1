b3264f87af1b8e9ea16582f72cf5777a96718a7c
Michael H <git@riskymh.dev>
2023-10-15 09:26:15 +1100

    Convert jobs and ping + delete other unused files (#7)

--- src/commands/slash/not-enough-info.ts
+++
@@ -1,9 +0,0 @@
-import { notEnoughInfoReply } from '../common-responses';
-import { createMentionableSlashCommand } from '../create-mentionable-slash-command';
-
-export const command = createMentionableSlashCommand({
-  name: 'not-enough-info',
-  description:
-    'Replies with directions for questions with not enough information',
-  reply: notEnoughInfoReply,
-});
--- src/commands/slash/promotion.ts
+++
@@ -1,8 +0,0 @@
-import { promotionResponse } from '../common-responses';
-import { createMentionableSlashCommand } from '../create-mentionable-slash-command';
-
-export const command = createMentionableSlashCommand({
-  name: 'promotion',
-  description: 'Replies with the server rules for promotion',
-  reply: promotionResponse,
-});
